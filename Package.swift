4168f565990f564297e96dd2c06cd0bbe9678ff2
Isaac Halvorson <hello@hisaac.net>
2021-06-29 15:17:18 -0500

    Change Swift NIO version in Package.swift to 2.30.0 (#7)

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         // Make sure to update the versions used in the `repositories.bzl` file if you change them here
         .package(url: "https://github.com/apple/swift-log", .exact("1.4.2")),
-        .package(url: "https://github.com/apple/swift-nio", .exact("2.30.2")),
+        .package(url: "https://github.com/apple/swift-nio", .exact("2.30.0")),
     ],
     targets: [
         .target(
