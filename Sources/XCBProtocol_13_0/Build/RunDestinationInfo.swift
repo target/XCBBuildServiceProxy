10e727a10fdd5562af8290a0580f5e93b87b6bff
Sean Berry <Sean.Berry@target.com>
2021-08-12 14:00:59 -0500

    update RunDestinationInfo comments

--- Sources/XCBProtocol_13_0/Build/RunDestinationInfo.swift
+++ Sources/XCBProtocol_13_0/Build/RunDestinationInfo.swift
@@ -4,9 +4,8 @@ import XCBProtocol
 
 public struct RunDestinationInfo: Decodable {
     public let platform: BuildPlatform // e.g. "macosx"
-    public let sdk: String
-    public let sdkVariant: String //  e.g. "macosx10.14"
-//    public let modernPlatform: String // e.g. "macos". This is a made up name. I couldn't find the correct one in https://github.com/keith/Xcode.app-strings/blob/master/Xcode.app/Contents/SharedFrameworks/XCBuild.framework/Versions/A/PlugIns/XCBBuildService.bundle/Contents/Frameworks/XCBProtocol.framework/Versions/A/XCBProtocol.
+    public let sdk: String //  e.g. "macosx10.14"
+    public let sdkVariant: String  //  e.g. "macos" or "iphonesimulator"
     public let targetArchitecture: String // e.g. "x86_64", "arm64"
     public let supportedArchitectures: [String] // e.g. ["armv7s", "arm64"]
     public let disableOnlyActiveArch: Bool
